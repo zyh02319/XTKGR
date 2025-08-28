3aa70bef96f3e1c97efe1dee3d72979c26a1b4b0
zyh <2365174401@qq.com>
2025-08-28 03:00:30 -0700

    修改模型管理界面

---
+++ build/CMakeFiles/KGR_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "KGR_autogen"
+  "CMakeFiles/KGR_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/KGR_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/KGR_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
